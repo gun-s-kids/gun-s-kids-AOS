a6a318cdf87e3d97cc8d4ab273d4d50865efc0c7
duhui <dh151446@gmail.com>
2023-09-30 00:30:56 +0900

    style : activity, fragment 종속성 추가
    viewModels 사용하기 위함

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -58,6 +58,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+//   hint 종속성 추가
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+//    by viewModels 종속성 추가
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
 }
