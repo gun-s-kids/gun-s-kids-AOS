9262ed451562a42b57aa549605adb3ade8aa1263
duhui <dh151446@gmail.com>
2023-09-30 00:52:17 +0900

    design : 계정찾기 액티비티 생성

---
+++ presentation/src/main/java/com/ehyundai/project/club/view/login/FindAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.ehyundai.project.club.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ehyundai.project.club.R
+
+class FindAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_account)
+    }
+}
