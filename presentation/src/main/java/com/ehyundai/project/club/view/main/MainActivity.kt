68e33b77d12f04daa3f433ad47a0ef0240099450
duhui <dh151446@gmail.com>
2023-10-24 22:16:39 +0900

    design : 하단 탭 디자인

--- presentation/src/main/java/com/ehyundai/project/club/view/main/MainActivity.kt
+++ presentation/src/main/java/com/ehyundai/project/club/view/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.ehyundai.project.club.R
 import com.ehyundai.project.club.base.BaseActivity
 import com.ehyundai.project.club.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override val TAG: String = "MainActivity"
@@ -12,5 +13,16 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         super.onCreate(savedInstanceState)
 
         binding.vpMain.adapter = ViewPager2Adapter(supportFragmentManager, lifecycle)
+        drawTabLayout()
+    }
+
+    private fun drawTabLayout(){
+        // connect ViewPager2
+        TabLayoutMediator(binding.tlMain, binding.vpMain) { _, _ -> }.attach()
+
+        // set tabIcon
+        binding.tlMain.getTabAt(0)!!.setIcon(R.drawable.ic_search_rank_down)
+        binding.tlMain.getTabAt(1)!!.setIcon(R.drawable.ic_search_no_change)
+        binding.tlMain.getTabAt(2)!!.setIcon(R.drawable.ic_search_rank_up)
     }
 }
