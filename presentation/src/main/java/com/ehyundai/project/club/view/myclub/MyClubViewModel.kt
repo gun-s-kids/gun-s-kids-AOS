28e0e5dc052100d3a04b22845a7bef515fba5f4c
duhui <dh151446@gmail.com>
2023-10-31 21:16:15 +0900

    asdf

--- presentation/src/main/java/com/ehyundai/project/club/view/myclub/MyClubViewModel.kt
+++ presentation/src/main/java/com/ehyundai/project/club/view/myclub/MyClubViewModel.kt
@@ -1,6 +1,28 @@
 package com.ehyundai.project.club.view.myclub
 
 import com.ehyundai.project.club.base.BaseViewModel
+import com.ehyundai.project.club.view.Club
 
 class MyClubViewModel : BaseViewModel() {
+
+////    var clubs : MutableList<Club> = mutableListOf()
+//    val size get() = clubs.size
+//
+////    TODO 클럽 추가 등 처리하기
+////    public fun addClub(message:String){
+////        clubs.add(message)
+////    }
+//
+//    public fun getClubs() : List<Club>{
+//        return clubs.toList()
+//    }
+//
+//    public fun getClubsAt(idx : Int) : Club{
+//        return clubs[idx]
+//    }
+//
+//    public fun removeClubAt(index:Int){
+//        clubs.removeAt(index)
+//    }
+
 }
