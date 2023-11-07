493542c62b1f32f8a776b7448073d52a472b2f3a
duhui <dh151446@gmail.com>
2023-11-07 22:43:09 +0900

    feat : 검색, 나의클럽 화면 1차

--- presentation/src/main/java/com/ehyundai/project/club/view/myclub/MyClubFragment.kt
+++ presentation/src/main/java/com/ehyundai/project/club/view/myclub/MyClubFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.viewModels
 import com.ehyundai.project.club.R
 import com.ehyundai.project.club.base.BaseFragment
 import com.ehyundai.project.club.databinding.FragmentMyClubBinding
+import com.ehyundai.project.club.view.Club
+import com.ehyundai.project.club.view.ClubAdapter
 import com.ehyundai.project.club.view.main.MainActivity
 
 class MyClubFragment : BaseFragment<FragmentMyClubBinding>(R.layout.fragment_my_club) {
@@ -14,13 +16,32 @@ class MyClubFragment : BaseFragment<FragmentMyClubBinding>(R.layout.fragment_my_
     private val viewModel: MyClubViewModel by viewModels()
     override val TAG: String = "MyClubFragment"
 
+    lateinit var context: MainActivity
+
+    /** dummy Data -> API 생성 시 삭제 */
+
+    var groupList = arrayListOf<Club>(
+        Club(R.drawable.ic_shinchan, "슛솔랭", "2022.10.10", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "스뚝파", "2022.10.16", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "러닝", "2011.09.10", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "자바락", "2022.11.04", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "밴드", "2022.11.04", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "뀨르", "2022.11.04", "안녕하세요"),
+    )
+
+    /** dummy Data -> API 생성 시 삭제 */
+
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         parentActivity = activity as MainActivity
+        this.context = context as MainActivity
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val adapter = ClubAdapter(context, groupList)
+        binding.lvClub.adapter = adapter
         binding.viewModel = viewModel
     }
 
--- presentation/src/main/java/com/ehyundai/project/club/view/search/SearchFragment.kt
+++ presentation/src/main/java/com/ehyundai/project/club/view/search/SearchFragment.kt
@@ -2,11 +2,14 @@ package com.ehyundai.project.club.view.search
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.ehyundai.project.club.R
 import com.ehyundai.project.club.base.BaseFragment
 import com.ehyundai.project.club.databinding.FragmentSearchBinding
+import com.ehyundai.project.club.view.Club
+import com.ehyundai.project.club.view.ClubAdapter
 import com.ehyundai.project.club.view.main.MainActivity
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
@@ -14,13 +17,37 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
     private val viewModel: SearchViewModel by viewModels()
     override val TAG: String = "SearchFragment"
 
+    lateinit var context: MainActivity
+
+    /** dummy Data -> API 생성 시 삭제 */
+
+    var groupList = arrayListOf<Club>(
+        Club(R.drawable.ic_shinchan, "슛솔랭", "2022.10.10", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "스뚝파", "2022.10.16", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "러닝", "2011.09.10", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "자바락", "2022.11.04", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "밴드", "2022.11.04", "안녕하세요"),
+        Club(R.drawable.ic_shinchan, "뀨르", "2022.11.04", "안녕하세요"),
+    )
+
+    /** dummy Data -> API 생성 시 삭제 */
+
     override fun onAttach(context: Context) {
         super.onAttach(context)
         parentActivity = activity as MainActivity
+        this.context = context as MainActivity
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        for (i in 0 until 10){
+            val view = LayoutInflater.from(context).inflate(R.layout.item_company_view, null)
+            binding.llSortCompany.addView(view)
+        }
+        val adapter = ClubAdapter(context, groupList)
+        binding.lvClub.adapter = adapter
+
         binding.viewModel = viewModel
     }
 
